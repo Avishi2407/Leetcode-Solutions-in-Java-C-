b0562d387e55a39b573c2e700998f646cb8263ec
Avishi Jain <96139314+Avishi2407@users.noreply.github.com>
2024-05-14 02:46:35 +0530

    Time: 3 ms (60.51%) | Memory: 9.1 MB (17.96%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        map<char,int> mp;
+        
+        for(auto it : s){
+            mp[it]++;
+        }
+        for(auto it : t){
+            mp[it]++;
+        
+        }
+        for(auto it : mp){
+            if(it.second % 2 != 0){
+                return it.first;
+            }
+        }
+        return ' ';
+    }
+};
