7a51274b3ca6818334fd865ed1af278b96fe6388
Avishi Jain <96139314+Avishi2407@users.noreply.github.com>
2024-05-14 02:57:51 +0530

    Time: 4 ms (48.03%) | Memory: 7.8 MB (54.99%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char,int> mp;
-        
-        for(auto it : s){
-            mp[it]++;
-        }
-        for(auto it : t){
-            mp[it]++;
-        
-        }
-        for(auto it : mp){
-            if(it.second % 2 != 0){
-                return it.first;
-            }
-        }
-        return ' ';
+        vector<int> mpp(26,0);
+        const int m = s.size();const int n = t.size();
+        for(int i=0;i<n;++i){mpp[t[i]-'a']++;}
+        for(int i=0;i<m;++i){mpp[s[i]-'a']--;}
+        for(int i=0;i<26;++i){if(mpp[i]==1){return i+'a';}}
+        return 'a';
     }
 };
