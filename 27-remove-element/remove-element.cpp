2200c5b468f5bd55110af103fd69ae2072b10275
Avishi Jain <96139314+Avishi2407@users.noreply.github.com>
2024-05-29 15:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.81%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int removeElement(std::vector<int>& nums, int val) {
-       int x =0; 
-        for(int i =0;i<nums.size(); i++){
-            if(nums[i] != val){
-                nums[x] = nums[i];
-                x++;
-            }
-        }
-        return x;
+       auto newEnd = std::remove(nums.begin(), nums.end(), val);
+    
+    // Calculate the new size after removal
+    int newSize = std::distance(nums.begin(), newEnd);
+    
+    // Resize the vector to the new size
+    nums.resize(newSize);
+    
+    return newSize;
     }
 };
