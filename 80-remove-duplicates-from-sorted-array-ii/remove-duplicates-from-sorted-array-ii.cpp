10258d8855865905742b99329554065fe2059110
Avishi Jain <96139314+Avishi2407@users.noreply.github.com>
2024-05-31 00:10:37 +0530

    Time: 4 ms (66.64%) | Memory: 13.4 MB (39.94%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        map<int, int> countMap;
-
-        // Step 1: Count the occurrences of each element
-        for (int num : nums) {
-            countMap[num]++;
-        }
-
-        // Step 2: Rebuild the array allowing each element at most twice
-        int index = 0;
-        for (auto& [num, count] : countMap) {
-            int times = min(count, 2); // Limit the count to at most 2
-            for (int i = 0; i < times; ++i) {
-                nums[index++] = num;
+        int i =0;
+        for(auto ele:nums){
+            if(i<2 || nums[i-2]!=ele){
+                nums[i] = ele;
+                i++;
             }
         }
-
-        // Return the new length of the array
-        return index;
+        return i;
     }
 };
