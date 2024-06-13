9588ed1d78ff649e811b0f113590ea8602c4368f
Avishi Jain <96139314+Avishi2407@users.noreply.github.com>
2024-06-13 11:48:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (34.22%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,7 +2,16 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-          sort(nums.begin(), nums.end()); 
+        int arr[3] = {0};
+        for(int ele: nums)
+            arr[ele]++;
+        
+        int n = nums.size();
+        int count =0;
+        for(int i =0; i< 3; i++){
+            fill(nums.begin()+count , nums.begin()+count+ arr[i], i);
+            count = count + arr[i];
+        }
         
     }
 };
