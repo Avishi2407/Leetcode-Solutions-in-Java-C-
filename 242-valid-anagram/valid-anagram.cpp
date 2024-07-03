09d4d049c6e8e83df9a61c2ecf397f4bbb36bb25
Avishi Jain <96139314+Avishi2407@users.noreply.github.com>
2024-07-03 15:26:15 +0530

    Time: 10 ms (45.89%) | Memory: 10.1 MB (5.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,22 +5,16 @@ public:
             return false;
         }
         map<char,int> mp1;
-        map<char, int> mp2;
-        
+         
         for(auto ele: s){
             mp1[ele]++;
         }
         for(auto ele: t){
-            mp2[ele]++;
+            mp1[ele]--;
         }
         
-        for(int i =0; i< s.size() ; i++){
-            if(mp2.find(s[i]) != mp2.end()){
-                if(mp1[s[i]] != mp2[s[i]]){
-                    return false;
-                }
-            }
-            else{
+        for(int i =0; i<mp1.size(); i++){
+            if(mp1[i] != 0){
                 return false;
             }
         }
